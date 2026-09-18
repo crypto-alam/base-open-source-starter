055cddc4ae8c7280956204714fc3b12b268066d5
crypto-alam <tahira4038571@gmail.com>
2026-09-18 12:59:25 +0500

    test: cover number reset behavior

--- test/SimpleStorage.test.js
+++ test/SimpleStorage.test.js
@@ -9,4 +9,14 @@ describe("SimpleStorage", function () {
 
     expect(await simpleStorage.getNumber()).to.equal(42);
   });
+
+  it("should reset the stored number to zero", async function () {
+    const SimpleStorage = await ethers.getContractFactory("SimpleStorage");
+    const simpleStorage = await SimpleStorage.deploy();
+
+    await simpleStorage.setNumber(100);
+    await simpleStorage.resetNumber();
+
+    expect(await simpleStorage.getNumber()).to.equal(0);
+  });
 });
