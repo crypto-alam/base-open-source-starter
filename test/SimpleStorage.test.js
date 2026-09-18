9193e339bf648a250254c12e2fac20cd90f6a78e
crypto-alam <tahira4038571@gmail.com>
2026-09-18 12:58:27 +0500

    test: add initial SimpleStorage test

---
+++ test/SimpleStorage.test.js
@@ -0,0 +1,12 @@
+const { expect } = require("chai");
+
+describe("SimpleStorage", function () {
+  it("should store and return a number", async function () {
+    const SimpleStorage = await ethers.getContractFactory("SimpleStorage");
+    const simpleStorage = await SimpleStorage.deploy();
+
+    await simpleStorage.setNumber(42);
+
+    expect(await simpleStorage.getNumber()).to.equal(42);
+  });
+});
